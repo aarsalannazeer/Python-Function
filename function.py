d5f0098b4a7f5bad57725083bdc747c02544bbc1
arslan <aarsalannazeer@gmail.com>
2020-04-25 02:55:13 +0500

    Initial commit

---
+++ function.py
@@ -0,0 +1,15 @@
+def addition(x,y):
+    z=a+b
+    return print(z)
+a=input("enter input1: ")
+b=input("enter input2: ")
+a=int(a)
+b=int(b)
+addition(a,b)
+
+def Celcius_to_fah(p):
+    f=(9/5)*c+32
+    return print(f)
+c=input("enter the temp in celcius: ")
+c=int(c)
+Celcius_to_fah(c)
